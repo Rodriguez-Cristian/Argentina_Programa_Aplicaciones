0027fa0f7c5e10d5a7914038195cd1edcb0a48ca
Cristian Gabriel Rodriguez <rodriguezcristiangabriel@outlook.com>
2023-08-21 20:45:10 -0300

    Tablas
    Permite Cargar y Eliminar Tablas

---
+++ Tablas/src/tablas/Alumno.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tablas;
+
+/**
+ *
+ * @author Cristian Rodriguez
+ */
+public class Alumno {
+    
+    private int matricula;
+    private String apellido;
+    private String nombre;
+
+    public Alumno(int matricula, String apellido, String nombre) {
+        this.matricula = matricula;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
